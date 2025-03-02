aeb3dde55fde648e8f4b99dcec420d7e96621322
vinsfortunato <vinsfortunato97@gmail.com>
2025-03-02 13:25:57 +0100

    feat(network): check if connected to BlueNet VPN by pinging the server when updating ipaddr file.

--- src/usr/libexec/ipaddr-listener.sh
+++ src/usr/libexec/ipaddr-listener.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 IP_FILE="/run/user/1000/itg/ipaddr"
-CHECK_INTERVAL=5  # Time in seconds to wait between each check for IP address changes
+CHECK_INTERVAL=20  # Time in seconds to wait between each check for IP address changes
+BNET_SERVER_IP="10.0.0.1"
+BNET_SERVER_PING_TIMEOUT=2
 
 get_current_ips() {
     ip -o -4 addr show | awk '{if ($3 != "lo") print $2 "=" $4}' | cut -d'/' -f1
@@ -10,14 +12,12 @@ get_current_ips() {
 CURRENT_IPS=$(get_current_ips)
 
 if [[ ! -f "$IP_FILE" ]]; then
-    # If the file doesn't exist, create it and write the initial IP addresses with interface names
     echo "$CURRENT_IPS" > "$IP_FILE"
     echo "Initial IP addresses set:"
     echo "$CURRENT_IPS"
 else
     LAST_IPS=$(cat "$IP_FILE")
 
-    # If the IP addresses have changed, update the file
     if [[ "$CURRENT_IPS" != "$LAST_IPS" ]]; then
         echo "$CURRENT_IPS" > "$IP_FILE"
         echo "IP addresses updated:"
@@ -29,11 +29,15 @@ while true; do
     CURRENT_IPS=$(get_current_ips)
     LAST_IPS=$(cat "$IP_FILE")
 
-    # If the IP addresses have changed, update the file
     if [[ "$CURRENT_IPS" != "$LAST_IPS" ]]; then
         echo "$CURRENT_IPS" > "$IP_FILE"
         echo "IP addresses updated:"
         echo "$CURRENT_IPS"
+    fi
+
+    # Check if connected to Bnet VPN server.
+    if ! ping -c 1 -W "$BNET_SERVER_PING_TIMEOUT" "$BNET_SERVER_IP" &>/dev/null; then
+        sed -i '/^bnet0=/d' "$IP_FILE"
     fi
 
     sleep "$CHECK_INTERVAL"
