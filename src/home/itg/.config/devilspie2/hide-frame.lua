22911bada5c71dbc23c5959863e3776ea01bd03c
vinsfortunato97 <vinsfortunato97@gmail.com>
2025-09-14 12:23:48 +0200

    fix(itgmania): fix game starting not centered when using windowed borderless mode with devilspie2.

--- src/home/itg/.config/devilspie2/hide-frame.lua
+++ src/home/itg/.config/devilspie2/hide-frame.lua
@@ -2,5 +2,16 @@
 
 if (get_window_name() == "Simply Love") then
     undecorate_window()
-    set_window_position(0, 0)
+
+    -- We'll check up to 10 times (1s apart) to ensure it's at (0,0)
+    for i = 1, 10 do
+        local x = get_window_x()
+        local y = get_window_y()
+
+        if x ~= 0 or y ~= 0 then
+            set_window_position(0, 0)
+            os.execute("sleep 1")
+        end
+    end
 end
+
